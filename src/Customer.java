853f6f510f47c8d393e143918d2ba06a232c6e03
jvtamm <jv_tamm@hotmail.com>
2019-11-06 20:50:54 -0300

    COMMIT #9

--- src/Customer.java
+++ src/Customer.java
@@ -43,7 +43,7 @@ class Customer {
         return result;
      }
 
-     private int getTotalFrequentRenterPoints(){
+    private int getTotalFrequentRenterPoints(){
         int result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
@@ -51,5 +51,23 @@ class Customer {
             result += each.getFrequentRenterPoints();
         }
         return result;
-     }
+    }
+
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        
+        // add footer lines
+        result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
 }
